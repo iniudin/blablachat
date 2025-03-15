1437857dac17a5948d6e90059d37556718a441b9
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-15 08:08:34 +0700

    feat: auth middleware

--- chat-server/app/controllers/application_controller.rb
+++ chat-server/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    token = request.headers['Authorization']&.split(' ').last
+    @current_user = User.find_by(auth_token: token)
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+  end
+
+  def current_user
+    @current_user
+  end
+
 end
