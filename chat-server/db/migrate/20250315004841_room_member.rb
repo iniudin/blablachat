bea36437f260c966e8264ca3571f58baa3004c33
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-15 07:56:49 +0700

    feat: create room members migration

---
+++ chat-server/db/migrate/20250315004841_room_member.rb
@@ -0,0 +1,12 @@
+class RoomMember < ActiveRecord::Migration[8.0]
+  def change
+    create_table :room_members do |t|
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :room_members, [:room_id, :user_id], unique: true
+  end
+end
