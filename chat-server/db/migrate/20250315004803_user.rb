dfd5a974ca79b8d3d36167cb62e03840094a0c93
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-15 07:52:41 +0700

    feat: create user migration

---
+++ chat-server/db/migrate/20250315004803_user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :password_digest, null: false
+      t.string :auth_token, null: false
+      t.timestamps
+    end
+
+    add_index :users, :name, unique: true
+    add_index :users, :auth_token, unique: true
+
+  end
+end
