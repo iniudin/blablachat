c28b14b601bdc414f47153d05f8139b1edc11c6f
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-15 07:59:19 +0700

    feat: create room message migration

---
+++ chat-server/db/migrate/20250315004856_message.rb
@@ -0,0 +1,13 @@
+class Message < ActiveRecord::Migration[8.0]
+  def change
+    create_table :messages do |t|
+      t.text :content, null: false
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :messages, [:room_id, :user_id], unique: true
+  end
+end
