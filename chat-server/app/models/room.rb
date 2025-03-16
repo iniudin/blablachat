ad799009b65570c163ba87d0922af28b8ae1a7fc
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-17 06:43:59 +0700

    feat: auto generate code on create room

--- chat-server/app/models/room.rb
+++ chat-server/app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
+  before_create :generate_invite_code
+
   belongs_to :owner, class_name: 'User'
   has_many :room_members, dependent: :destroy
   has_many :users, through: :room_members
@@ -7,4 +9,10 @@ class Room < ApplicationRecord
   validates :name, presence: true
   validates :invite_code, uniqueness: true
   validates :public, inclusion: { in: [true, false] }
+
+  private
+
+  def generate_invite_code
+    self.invite_code = SecureRandom.hex(6)
+  end
 end
