c0eb7d36212c7d9f62ad2c733ebea7ef54e9620e
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-17 05:42:18 +0700

    feat: default layout that handle session

---
+++ chat-nuxt/app/layouts/default.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const auth = useAuthStore();
+const router = useRouter();
+
+watchEffect(() => {
+  if (!auth.isLoggedIn) {
+    router.push('/login');
+  }
+});
+</script>
+
+<template>
+  <UContainer class="py-4">
+    <div class="flex items-center justify-between">
+      <h1 class="font-bold text-2xl text-(--ui-primary) mb-4">
+        Chats
+      </h1>
+      <div
+        v-if="auth.isLoggedIn"
+        class="flex items-center gap-4"
+      >
+        <UButton
+          icon="i-lucide-user-plus"
+          label="New Chat"
+          to="/chats/new"
+        />
+        <UButton
+          icon="i-lucide-user"
+          label="Logout"
+          @click="auth.logout"
+        />
+      </div>
+    </div>
+    <slot />
+  </UContainer>
+</template>
