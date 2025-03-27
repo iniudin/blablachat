15f92b92141f9949b30a998e8464807724cf0132
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-27 19:01:46 +0700

    fix: use localstorage for authstore

--- chat-nuxt/app/stores/useAuthStore.ts
+++ chat-nuxt/app/stores/useAuthStore.ts
@@ -1,20 +1,31 @@
-import { defineStore } from 'pinia';
-import type { User } from '~/types';
+import { defineStore } from "pinia";
+import type { User } from "~/types";
 
-export const useAuthStore = defineStore('auth', () => {
-  const user = useCookie<User | null>('user', { default: () => null });
-  const token = useCookie<string | null>('token', { default: () => null });
+export const useAuthStore = defineStore("auth", () => {
+  const user = ref<User | null>(
+    JSON.parse(localStorage.getItem("user") || "null"),
+  );
+
+  const token = ref<string | null>(
+    localStorage.getItem("token"),
+  );
 
   const isLoggedIn = computed(() => !!user.value && !!token.value);
 
   function login(newUser: User, newToken: string) {
     user.value = newUser;
     token.value = newToken;
+
+    localStorage.setItem("user", JSON.stringify(newUser));
+    localStorage.setItem("token", newToken);
   }
 
   function logout() {
     user.value = null;
     token.value = null;
+
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
   }
 
   return { user, token, isLoggedIn, login, logout };
