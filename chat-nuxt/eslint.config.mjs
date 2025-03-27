ae49a42886603bbc8663752d2d764a3c480799cd
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-27 18:02:11 +0700

    chore: eslint config

--- chat-nuxt/eslint.config.mjs
+++ chat-nuxt/eslint.config.mjs
@@ -1,4 +1,31 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt(
+  {
+    rules: {
+      "object-curly-spacing": [
+        "error", "always",
+      ],
+      "eol-last": [
+        "error", "always",
+      ],
+      "no-trailing-spaces": ["error"],
+      "require-await": ["error"],
+      "vue/html-self-closing": [
+        "error",
+        {
+          html: {
+            void: "always",
+            normal: "never",
+            component: "always",
+          },
+          svg: "never",
+          math: "never",
+        },
+      ],
+      "@stylistic/quotes": [
+        "warn", "double", { avoidEscape: true },
+      ],
+    },
+  });
--- chat-nuxt/nuxt.config.ts
+++ chat-nuxt/nuxt.config.ts
@@ -1,28 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@pinia/nuxt"],
+  modules: [
+    "@nuxt/ui", "@nuxt/eslint", "@pinia/nuxt",
+  ],
   devtools: {
-    enabled: true
-  },
-  eslint: {
-    config: {
-      stylistic: {
-        indent: 2,
-        semi: true,
-      }
-    }
-  },
-  routeRules: {
-    "/chats": { ssr: false },
-  },
-  runtimeConfig: {
-    public: {
-      apiBase: process.env.API_BASE || "http://localhost:3001",
-    },
+    enabled: true,
   },
   css: ["~/assets/css/main.css"],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE || "http://localhost:3000",
+    },
+  },
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: "2024-11-27",
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        quotes: "double",
+        commaDangle: "always-multiline",
+        jsx: false,
+        braceStyle: "1tbs",
+      },
+    },
+  },
 });
