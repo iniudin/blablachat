04e1e468a2246186a84a7f94662f6e0539fdb249
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-27 18:01:36 +0700

    fix: room and channel

--- chat-nuxt/app/utils/rooms.ts
+++ chat-nuxt/app/utils/rooms.ts
@@ -1,23 +1,54 @@
-import type { Room } from '~/types';
+import type { Room } from "~/types";
 
 const getRooms = async () => {
-  return await useApiFetch<Room[]>('/api/rooms');
+  return await $fetch<Room[]>("/api/rooms", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 };
 
 const getRoom = async (id: string) => {
-  return await useApiFetch<Room>(`/api/rooms/${id}`);
+  return await $fetch<Room>(`/api/rooms/${id}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 };
 
 const createRoom = async (data: Partial<Room>) => {
-  return await useApiFetch<Room>('/api/rooms', { method: 'POST', body: data });
+  return await $fetch<Room>("/api/rooms", {
+    method: "POST",
+    body: data,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 };
 
-const joinRoom = async (id: string) => {
-  return await useApiFetch<Room>(`/api/rooms/${id}/join`, { method: 'POST' });
+const joinRoom = async (id: string, inviteCode?: string) => {
+  if (inviteCode) {
+    return await $fetch<Room>(`/api/rooms/invite?invite_code=${inviteCode}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+  }
+  return await $fetch<Room>(`/api/rooms/${id}/join`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 };
 
 const leaveRoom = async (id: string) => {
-  return await useApiFetch<Room>(`/api/rooms/${id}/leave`, { method: 'POST' });
+  return await $fetch<Room>(`/api/rooms/${id}/leave`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 };
 
 export {
