030a9a6d171d3e510543c77358cea457ba9a25b6
Ahmad Syaifudin <sysfdn@pm.me>
2025-03-29 03:09:57 +0700

    fix: allow action cable request for specific origins at production.rb

--- chat-server/config/environments/production.rb
+++ chat-server/config/environments/production.rb
@@ -84,4 +84,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Allow requests from specific origins for Action Cable.
+  config.action_cable.allowed_request_origins = ["https://chat.udin.web.id", "http://chat.udin.web.id"]
 end
